39c917772607baeea8335dffd2602f02c9c7ea2f
Up <upcraftedlp@gmail.com>
2025-02-19 13:36:20 +0100

    switch to MDG

---
+++ Forge/src/main/java/dev/upcraft/origins/icarae/forge/power/WingsPowerFactory.java
@@ -0,0 +1,10 @@
+package dev.upcraft.origins.icarae.forge.power;
+
+import io.github.edwinmindcraft.apoli.api.power.factory.PowerFactory;
+
+public class WingsPowerFactory extends PowerFactory<WingsPower> {
+
+    public WingsPowerFactory() {
+        super(WingsPower.CODEC);
+    }
+}
